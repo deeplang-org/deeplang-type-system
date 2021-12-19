42928ec3e99ef1cdaeefbf6dd909ed0723c39974
Mepy <angel@mepy.net>
2021-12-19 23:07:12 +0800

    Update example.ml
    symbol table structure v0.1.1 #add some comments

--- table/example.ml
+++ table/example.ml
@@ -18,6 +18,22 @@ type dcl_tab_ent =
 ;;
 let print_key int dte = print_string ( (Int.to_string int) ^ "\n" );;
 
+
+(* 
+walk1 : code -> dcl_tab 
+[code] 
+
+increment style : code -> old_tab -> new_tab
+walk1 : code -> tab(mutable) -> unit  ::: We might use this 
+
+type table = {
+    dcl:dcl_tab
+    ref:ref_tab
+}
+or dcl_tab * ref_tab 
+
+*)
+
 let rec tableCode (e:AbsDeeplang.code) : dcl_tab_ent Dcl_Tab.t = match e with
        AbsDeeplang.Declare declare -> tableDeclare declare 
   |    AbsDeeplang.Define define -> tableDefine define
@@ -41,16 +57,16 @@ and tableDeclare (e:AbsDeeplang.declare) : dcl_tab_ent Dcl_Tab.t = match e with
         } Dcl_Tab.empty
   |    AbsDeeplang.DecFunc (varid, args, rettype) -> Dcl_Tab.empty
   |    AbsDeeplang.Interface (interfacename, methods) -> Dcl_Tab.empty
+(* let x = 1 : dcl_tab *)
 
 and tableDefine (e:AbsDeeplang.define) = Dcl_Tab.empty
-
-
-
+(* func body code list : recursively call walk1 function *)
 and tableStatement (e:AbsDeeplang.statement) = Dcl_Tab.empty
-
+(* [code] aka code list : recursively call walk1 function *)
 
 
 and tableExpression (e:AbsDeeplang.expression) = Dcl_Tab.empty
+(* code list X, expression : ref_tab *)
 ;;
 
 let node : AbsDeeplang.code = 
