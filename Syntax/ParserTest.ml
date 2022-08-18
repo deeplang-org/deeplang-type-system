af9aa3a957036b054ae253b36488bad4bde8a259
mmzk1526 <yc4120@ic.ac.uk>
2022-08-18 22:35:20 +0800

    [mmzk] (feat) 报错不匹配括号

--- Syntax/ParserTest.ml
+++ Syntax/ParserTest.ml
@@ -42,6 +42,9 @@ let _ =
                                 ; "test/expressions/bad_method_access2.dp"
                                 ; "test/expressions/bad_binary_op.dp"
                                 ; "test/expressions/bad_binary_op2.dp"
-                                ; "test/patterns/bad_match_pattern.dp" ])
+                                ; "test/expressions/mismatched_parens.dp"
+                                ; "test/expressions/mismatched_parens2.dp"
+                                ; "test/expressions/mismatched_parens3.dp"
+                                ; "test/expressions/mismatched_brack.dp" ])
         then (Format.printf "OK!\n"; exit 0)
         else (Format.printf "Not OK!\n"; exit 1)
