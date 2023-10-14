508b8ee0eb8994e357dfcb9f9e83a3f30d232758
mmzk1526 <yc4120@ic.ac.uk>
2023-10-14 13:58:10 +0100

    [mmzk] (refactor)把ANF逻辑放到IR文件夹下

--- IR/Playground.ml
+++ IR/Playground.ml
@@ -1,17 +1,10 @@
-module OrderedString = struct
-  type t = string
-  let compare = Stdlib.compare
-end
-
-module StringMap = Map.Make(OrderedString)
-
 type top_clause = Syntax.ParseTree.top_clause
 type func_impl = Syntax.ParseTree.func_impl
 type func_arg = Syntax.ParseTree.func_arg
 type context = Semantics.Walker.context
 type int_typ_sign = Syntax.ParseTree.int_typ_sign
 
-let trans_func_impl (_context:context) (func_impl:func_impl): Semantics.ANF.function_definition =
+let trans_func_impl (_context:context) (func_impl:func_impl): IR.ANF.function_definition =
   let get_arg_symbol (func_arg:func_arg) = match func_arg.farg_symb with
     Symbol(symbol) -> symbol in
   let (func_decl, stmt) = func_impl in
@@ -19,11 +12,11 @@ let trans_func_impl (_context:context) (func_impl:func_impl): Semantics.ANF.func
       func_src = stmt.span;
       func_name = func_decl.func_decl_name;
       func_params = List.map get_arg_symbol func_decl.func_decl_args;
-      func_label = Semantics.ANF.gen_label();
+      func_label = IR.ANF.gen_label();
       func_body = Return(stmt.span, Val(Int(114514)));
     };;
 
-let trans_top_clause (context:context) (top_clause:top_clause) : Semantics.ANF.function_definition = match top_clause.shape with
+let trans_top_clause (context:context) (top_clause:top_clause) : IR.ANF.function_definition = match top_clause.shape with
   | FunctionDef(func_impl) -> trans_func_impl(context)(func_impl)
   | _ -> failwith "TODO";;
 
@@ -84,4 +77,4 @@ let ast = clauses file;;
 
 let _ = List.iter iterator ast;;
 
-let _ = List.map (fun x -> print_endline (Semantics.ANF.show_function_definition(trans_top_clause(context)(x)))) ast;;
+let _ = List.map (fun x -> print_endline (IR.ANF.show_function_definition(trans_top_clause(context)(x)))) ast;;
