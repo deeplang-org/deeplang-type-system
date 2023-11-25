ede952988967282bf4d6475fef9c39c9893d68e3
mmzk1526 <yc4120@ic.ac.uk>
2023-11-25 13:56:46 +0000

    实现StmtSeq的IR转换

--- IR/Conversion.ml
+++ IR/Conversion.ml
@@ -17,16 +17,19 @@ let trans_expr (_context: context) (expr: Syntax.ParseTree.expr): ANF.expr = mat
     | LitString(str) -> Val(String(str)))
   | ExpVar(var_name) -> (match Hashtbl.find_opt _context.nametbl var_name with
     | Some(sym) ->  (match sym with
-      |Symbol(id) -> Val(Int(id)))
+      | Symbol(id) -> Val(Int(id)))
     | None -> failwith "find no variable")
   | _ -> failwith "TODO1";;
 
-let rec trans_stmt (_context: context) (stmt: Syntax.ParseTree.stmt): ANF.program = match stmt.shape with
-  | StmtSeq(stmt_list) -> (match stmt_list with
-    | s::ss -> List.fold_left ANF.concat_program (trans_stmt(_context) s) (List.map (trans_stmt(_context)) ss)
-    | _ -> failwith "Empty stmt list")
-  | StmtExpr(expr) -> Return(expr.span, trans_expr(_context) (expr))
-  | _ -> failwith "TODO3";;
+let rec trans_stmt (_context: context) (stmt: Syntax.ParseTree.stmt) (cont: Syntax.ParseTree.stmt list): ANF.program = match stmt.shape with
+  | StmtSeq(stmt_list) -> trans_stmts (_context) (stmt_list) (cont)
+  | StmtReturn(expr) -> Return(expr.span, trans_expr(_context) (expr))
+  (* | StmtExpr(expr) -> Return(expr.span, trans_expr(_context) (expr)) *)
+  | _ -> failwith "TODO3"
+
+and trans_stmts (_context: context) (stmts: Syntax.ParseTree.stmt list) (cont: Syntax.ParseTree.stmt list): ANF.program = match (stmts @ cont) with
+  | []    -> failwith "empty stmts"
+  | s::ss -> trans_stmt (_context) s ss;;
 
 let trans_func_impl (_context:context) (func_impl:func_impl): ANF.function_definition =
   let get_arg_symbol (func_arg:func_arg) = match func_arg.farg_symb with
@@ -37,7 +40,7 @@ let trans_func_impl (_context:context) (func_impl:func_impl): ANF.function_defin
       func_name = func_decl.func_decl_name;
       func_params = List.map get_arg_symbol func_decl.func_decl_args;
       func_label = ANF.gen_label();
-      func_body = trans_stmt (_context) (stmt);
+      func_body = trans_stmt (_context) (stmt) ([]);
     };;
 
 let trans_top_clause (context:context) (top_clause:top_clause) : ANF.function_definition = match top_clause.shape with
